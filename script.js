1fccdfa3b41681cfd3eb79bb74011be6a32fd4a2
Bob Bopkins <bobbopkins@gmail.com>
2016-11-12 02:59:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function popup() {
 
-	for (var i = 0; i < 5; i++) {
-
-		var str = "You will be sent to a questionable website soon.";
-		window.alert(str);
-			
+	while (true) {
+	
+		window.alert("You will be taken to a questionable website soon.")
 	}
 }
